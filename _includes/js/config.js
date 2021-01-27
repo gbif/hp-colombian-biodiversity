1516450ea59cdfbdbc053e428fff4d53fe390d9f
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2021-01-27 22:49:19 +0100

    scoped according to projection desc

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -3,5 +3,5 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', ext
 }});
 
 var siteConfig = {
-  rootPredicate: { type: 'equals', key: 'taxonKey', value: 5 }
+  rootPredicate: { type: 'equals', key: 'countryCode', value: 'CO' }
 };
